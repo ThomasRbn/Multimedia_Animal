3277a79dfef13a1db27cbae09eefaede23bd0654
Thomas <robineau1u@etu.univ-lorraine.fr>
2023-11-30 18:01:57 +0100

    :sparkles: Add translations file

---
+++ src/Constants.js
@@ -0,0 +1,17 @@
+export let LANGUAGES = {
+    "English": "en-US",
+    "Francais": "fr-FR",
+    "Deutsch": "de-DE",
+    "Italiano": "it-IT",
+    "Español": "es-ES",
+}
+
+export let CAT_TRANSLATIONS = ["Cat", "Chat", "Katze", "Gatto", "Gato"]
+export let CHICKEN_TRANSLATIONS = ["Chicken", "Poulet", "Huhn", "Pollo", "Pollo"]
+export let COW_TRANSLATIONS = ["Cow", "Vache", "Kuh", "Mucca", "Vaca"]
+export let DINOSAUR_TRANSLATIONS = ["Dinosaur", "Dinosaure", "Dinosaurier", "Dinosauro", "Dinosaurio"]
+export let DOG_TRANSLATIONS = ["Dog", "Chien", "Hund", "Cane", "Perro"]
+export let FROG_TRANSLATIONS = ["Frog", "Grenouille", "Frosch", "Rana", "Rana"]
+export let GOOSE_TRANSLATIONS = ["Goose", "Oie", "Gans", "Oca", "Ganso"]
+export let HORSE_TRANSLATIONS = ["Horse", "Cheval", "Pferd", "Cavallo", "Caballo"]
+export let SHEEP_TRANSLATIONS = ["Sheep", "Mouton", "Schaf", "Pecora", "Oveja"]
