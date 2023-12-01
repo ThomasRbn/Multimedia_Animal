4823c96bd3e47137b32859b330374ec1582f5256
Azuras03 <nr03russo@gmail.com>
2023-12-01 11:03:17 +0100

    Fix de l'état du jeu qui ne changeait pas

--- src/functions/GameFunctions.js
+++ src/functions/GameFunctions.js
@@ -3,13 +3,13 @@ import {PLAY_STATE_PLAYING, PLAY_STATE_STOPPED} from "../Constants.js";
 
 export function playGame() {
     let playButton = document.getElementById("playButton");
-    if (gameState.gameState === PLAY_STATE_STOPPED) {
-        gameState.gameState = PLAY_STATE_PLAYING;
+    if (gameState.playState === PLAY_STATE_STOPPED) {
+        gameState.playState = PLAY_STATE_PLAYING;
         console.log("Game is playing");
         playButton.innerHTML = "Stop";
         return;
     }
-    gameState.gameState = PLAY_STATE_STOPPED;
+    gameState.playState = PLAY_STATE_STOPPED;
     console.log("Game is stopped");
     playButton.innerHTML = "Play";
 }
