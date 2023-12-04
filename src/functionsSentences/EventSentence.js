8c7a4ae943976d56b29b5086eb746f5c702b03ce
Azuras03 <nr03russo@gmail.com>
2023-12-04 18:13:09 +0100

    Initiation des méthodes pour la création et le remplissage des boutons/phrases

--- src/functionsSentences/EventSentence.js
+++ src/functionsSentences/EventSentence.js
@@ -1,7 +1,70 @@
-import {getRandomSentence} from "./SentencesFunctions.js";
+import {checkAnimal, getRandomSentence} from "./SentencesFunctions.js";
+import {getAnimalTranslation} from "../functions/LanguageFunctions.js";
+import {ALL_ANIMALS} from "../ConstantsAnimalsGame.js";
+import {gameState} from "../GameState.js";
+import {toggleAnimation} from "../functions/ColorFunctions.js";
 
-export function initPhraseARemplir(selectedLanguage){
-    let sentence = getRandomSentence(selectedLanguage);
-    let phraseARemplir = document.getElementById("phraseARemplir");
-    phraseARemplir.innerHTML = sentence;
+let phraseARemplir = document.getElementById("phraseARemplirText");
+let audioScore = new Audio("assets/sound/score.mp3")
+audioScore.volume = 0.5;
+let audioLose = new Audio("assets/sound/WrongSound.mp3")
+
+/**
+ * Initialise la phrase à remplir
+ * @param selectedLanguage
+ */
+export function initPhraseARemplir(selectedLanguage) {
+    phraseARemplir.innerHTML = getRandomSentence(selectedLanguage);
+}
+
+/**
+ * Initiation des animaux à ajouter, avec leurs boutons et leur mot associés
+ * @param selectedLanguage
+ */
+export function initAnimauxAAjouter(selectedLanguage) {
+    let animauxAAjouter = document.getElementById("animaux");
+    for (let i = 0; i < ALL_ANIMALS.length; i++) {
+        let currentAnimal = getAnimalTranslation(ALL_ANIMALS[i][0], selectedLanguage).toUpperCase();
+        let button = document.createElement("button");
+        button.innerHTML = currentAnimal;
+        animauxAAjouter.appendChild(button);
+    }
+}
+
+/**
+ * Méthode initiant les boutons lorsque l'on clique (en fonction de si la réponse est bonne ou non)
+ * @param selectedLanguage
+ */
+export function initClickingOnAnimalEvent(selectedLanguage) {
+    let animauxAAjouter = document.getElementById("animaux");
+    let animaux = animauxAAjouter.querySelectorAll("button");
+    for (let i = 0; i < animaux.length; i++) {
+        let animal = animaux[i];
+        animal.addEventListener('click', function () {
+            let alt = animal.innerHTML;
+            if(checkAnimal(alt)){
+                let speech = new SpeechSynthesisUtterance(animal.innerHTML);
+                speech.lang = selectedLanguage;
+                window.speechSynthesis.speak(speech);
+                phraseARemplir.innerHTML = gameState.completeSentence;
+                playAudio(audioScore);
+                toggleAnimation('green-border', animal)
+                return;
+            }
+            playAudio(audioLose)
+            toggleAnimation('red-border', animal)
+       })
+    }
+}
+
+/**
+ * Fonction permettant de jouer un son mis en paramètre
+ * @param audio
+ */
+export function playAudio(audio){
+    if(audio.played){
+        audio.pause();
+        audio.currentTime = 0;
+    }
+    audio.play();
 }
