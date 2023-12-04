a304e6bbf876661179141a68ab550ba95d227f70
Thomas <robineau1u@etu.univ-lorraine.fr>
2023-12-04 14:57:23 +0100

    :sparkles: Add others translations

--- src/ConstantsSentenceGame.js
+++ src/ConstantsSentenceGame.js
@@ -4,31 +4,49 @@ const CAT_SENTENCES = [
     "My CAT enjoy hunting mice."
 ]
 
-let CHICKEN_SENTENCES = [
+const CHICKEN_SENTENCES = [
     "The CHICKEN is clucking in the backyard.",
-    "Chickens peck at the grains on the ground.",
+    "A CHICKEN peck at the grains on the ground.",
     "The CHICKEN lays eggs.",
 ];
 
-let DINOSAUR_SENTENCES = [
+const COW_SENTENCES = [
+    "The COW grazes in the meadow.",
+    "A COW provide milk for many dairy products.",
+    "The calf is the baby of the COW",
+];
+
+const DOG_SENTENCES = [
+    "The DOG barks loudly in the yard.",
+    "The DOG is known for his loyalty to humans.",
+    "The owner walks the DOG in the park every morning.",
+];
+
+const GOOSE_SENTENCES = [
+    "GOOSE sounds like honk.",
+    "My father is making foie gras from GOOSE.",
+    "The farmer have a very silly GOOSE",
+];
+
+const DINOSAUR_SENTENCES = [
     "The DINOSAUR is a prehistoric animal.",
     "The humans and the DINOSAUR did not live at the same time.",
     "The DINOSAUR has tiny arms.",
 ];
 
-let FROG_SENTENCES = [
+const FROG_SENTENCES = [
     "The FROG is jumping in the pond.",
     "Flies are the FROG's favorite food.",
     "When the FROG is young, it is called a tadpole.",
 ];
 
-let HORSE_SENTENCES = [
+const HORSE_SENTENCES = [
     "The HORSE is galloping in the field.",
     "Some humans ride a HORSE.",
     "Carrots are the HORSE's favorite food.",
 ];
 
-let SHEEP_SENTENCES = [
+const SHEEP_SENTENCES = [
     "The SHEEP is being sheared.",
     "Farmers raise SHEEP for their wool.",
     "The SHEEP is grazing in the field.",
