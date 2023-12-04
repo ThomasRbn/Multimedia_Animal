1bf3d808bd58c13ffd532bc9e796fe4e478861e5
Thomas <robineau1u@etu.univ-lorraine.fr>
2023-12-04 15:02:10 +0100

    :sparkles: Update constants style

--- src/ConstantsSentenceGame.js
+++ src/ConstantsSentenceGame.js
@@ -1,53 +1,245 @@
-const CAT_SENTENCES = [
-    "The CAT is playing with a ball of yarn.",
-    "The CAT loves to bask in the sun.",
-    "My CAT enjoy hunting mice."
-]
-
-const CHICKEN_SENTENCES = [
-    "The CHICKEN is clucking in the backyard.",
-    "A CHICKEN peck at the grains on the ground.",
-    "The CHICKEN lays eggs.",
-];
-
-const COW_SENTENCES = [
-    "The COW grazes in the meadow.",
-    "A COW provide milk for many dairy products.",
-    "The calf is the baby of the COW",
-];
-
-const DOG_SENTENCES = [
-    "The DOG barks loudly in the yard.",
-    "The DOG is known for his loyalty to humans.",
-    "The owner walks the DOG in the park every morning.",
-];
-
-const GOOSE_SENTENCES = [
-    "GOOSE sounds like honk.",
-    "My father is making foie gras from GOOSE.",
-    "The farmer have a very silly GOOSE",
-];
-
-const DINOSAUR_SENTENCES = [
-    "The DINOSAUR is a prehistoric animal.",
-    "The humans and the DINOSAUR did not live at the same time.",
-    "The DINOSAUR has tiny arms.",
-];
-
-const FROG_SENTENCES = [
-    "The FROG is jumping in the pond.",
-    "Flies are the FROG's favorite food.",
-    "When the FROG is young, it is called a tadpole.",
-];
-
-const HORSE_SENTENCES = [
-    "The HORSE is galloping in the field.",
-    "Some humans ride a HORSE.",
-    "Carrots are the HORSE's favorite food.",
-];
-
-const SHEEP_SENTENCES = [
-    "The SHEEP is being sheared.",
-    "Farmers raise SHEEP for their wool.",
-    "The SHEEP is grazing in the field.",
-];
+export const SENTENCES_TRANSLATIONS = {
+    CAT: {
+        en: [
+            "The CAT is playing with a ball of yarn.",
+            "The CAT loves to bask in the sun.",
+            "My CAT enjoy hunting mice."
+        ],
+        fr: [
+            "Le CHAT joue avec une pelote de laine.",
+            "Le CHAT aime se prélasser au soleil.",
+            "Mon CHAT aime chasser les souris."
+        ],
+        de: [
+            "Die KATZE spielt mit einem Wollknäuel.",
+            "Die KATZE sonnt sich gerne.",
+            "Meine KATZE jagt gerne Mäuse."
+        ],
+        it: [
+            "Il GATTO gioca con una palla di lana.",
+            "Il GATTO ama crogiolarsi al sole.",
+            "Il mio GATTO ama cacciare i topi."
+        ],
+        es: [
+            "El GATO juega con una bola de lana.",
+            "El GATO ama tomar el sol.",
+            "Mi GATO disfruta cazando ratones."
+        ]
+    },
+    CHICKEN: {
+        en: [
+            "The CHICKEN is clucking in the backyard.",
+            "A CHICKEN pecks at the grains on the ground.",
+            "The CHICKEN lays eggs."
+        ],
+        fr: [
+            "La POULE caquette dans la cour.",
+            "Une POULE picore les grains sur le sol.",
+            "La POULE pond des œufs."
+        ],
+        de: [
+            "Das HUHN gackert im Hinterhof.",
+            "Ein HUHN pickt Körner vom Boden.",
+            "Das HUHN legt Eier."
+        ],
+        it: [
+            "La GALLINA starnazza nel cortile.",
+            "Una GALLINA becca i grani per terra.",
+            "La GALLINA depone le uova."
+        ],
+        es: [
+            "La GALLINA cacarea en el patio trasero.",
+            "Una GALLINA pica los granos en el suelo.",
+            "La GALLINA pone huevos."
+        ]
+    },
+    COW: {
+        en: [
+            "The COW grazes in the meadow.",
+            "A COW provides milk for many dairy products.",
+            "The calf is the baby of the COW."
+        ],
+        fr: [
+            "La VACHE broute dans le pré.",
+            "Une VACHE fournit du lait pour de nombreux produits laitiers.",
+            "Le veau est le petit de la VACHE."
+        ],
+        de: [
+            "Die KUH grast auf der Wiese.",
+            "Eine KUH liefert Milch für viele Milchprodukte.",
+            "Das Kalb ist das Baby der KUH."
+        ],
+        it: [
+            "La MUCCA pascola nel prato.",
+            "Una MUCCA fornisce latte per molti prodotti lattieri.",
+            "Il vitello è il cucciolo della MUCCA."
+        ],
+        es: [
+            "La VACA pasta en el prado.",
+            "Una VACA proporciona leche para muchos productos lácteos.",
+            "El ternero es el bebé de la VACA."
+        ]
+    },
+    DOG: {
+        en: [
+            "The DOG barks loudly in the yard.",
+            "The DOG is known for his loyalty to humans.",
+            "The owner walks the DOG in the park every morning."
+        ],
+        fr: [
+            "Le CHIEN aboie bruyamment dans la cour.",
+            "Le CHIEN est connu pour sa fidélité envers les humains.",
+            "Le propriétaire promène le CHIEN au parc tous les matins."
+        ],
+        de: [
+            "Der HUND bellt laut im Hof.",
+            "Der HUND ist für seine Treue zu den Menschen bekannt.",
+            "Der Besitzer führt den HUND jeden Morgen im Park spazieren."
+        ],
+        it: [
+            "Il CANE abbaia forte nel cortile.",
+            "Il CANE è noto per la sua fedeltà agli umani.",
+            "Il proprietario porta a spasso il CANE ogni mattina nel parco."
+        ],
+        es: [
+            "El PERRO ladra fuerte en el patio.",
+            "El PERRO es conocido por su lealtad hacia los humanos.",
+            "El dueño pasea al PERRO en el parque todas las mañanas."
+        ]
+    },
+    GOOSE: {
+        en: [
+            "GOOSE sounds like honk.",
+            "My father is making foie gras from GOOSE.",
+            "The farmer has a very silly GOOSE."
+        ],
+        fr: [
+            "L'OIE fait un son comme honk.",
+            "Mon père prépare du foie gras à partir de l'OIE.",
+            "Le fermier a une OIE très drôle."
+        ],
+        de: [
+            "GÄNSE klingen wie honk.",
+            "Mein Vater macht Gänseleberpastete aus GÄNSEN.",
+            "Der Bauer hat eine sehr alberne GANS."
+        ],
+        it: [
+            "L'OCA suona come honk.",
+            "Mio padre sta preparando il foie gras dall'OCA.",
+            "Il contadino ha un'OCA molto sciocca."
+        ],
+        es: [
+            "El GANSO suena como honk.",
+            "Mi padre está haciendo foie gras de GANSO.",
+            "El granjero tiene un GANSO muy tonto."
+        ]
+    },
+    DINOSAUR: {
+        en: [
+            "The DINOSAUR is a prehistoric animal.",
+            "Humans and DINOSAURS did not live at the same time.",
+            "The DINOSAUR has tiny arms."
+        ],
+        fr: [
+            "Le DINOSAURE est un animal préhistorique.",
+            "Les humains et les DINOSAURES ne vivaient pas à la même époque.",
+            "Le DINOSAURE a de petits bras."
+        ],
+        de: [
+            "Der DINOSAURIER ist ein prähistorisches Tier.",
+            "Menschen und DINOSAURIER lebten nicht zur gleichen Zeit.",
+            "Der DINOSAURIER hat winzige Arme."
+        ],
+        it: [
+            "Il DINOSAURO è un animale preistorico.",
+            "Gli umani e i DINOSAURI non vivevano contemporaneamente.",
+            "Il DINOSAURO ha piccole braccia."
+        ],
+        es: [
+            "El DINOSAURIO es un animal prehistórico.",
+            "Los humanos y los DINOSAURIOS no vivieron al mismo tiempo.",
+            "El DINOSAURIO tiene brazos pequeños."
+        ]
+    },
+    FROG: {
+        en: [
+            "The FROG is jumping in the pond.",
+            "Flies are the FROG's favorite food.",
+            "When the FROG is young, it is called a tadpole."
+        ],
+        fr: [
+            "La GRENOUILLE saute dans l'étang.",
+            "Les mouches sont la nourriture préférée de la GRENOUILLE.",
+            "Quand la GRENOUILLE est jeune, on l'appelle un têtard."
+        ],
+        de: [
+            "Der FROSCH springt im Teich.",
+            "Fliegen sind die Lieblingsspeise des FROSCHS.",
+            "Wenn der FROSCH jung ist, nennt man ihn Kaulquappe."
+        ],
+        it: [
+            "La RANA salta nel laghetto.",
+            "Le mosche sono il cibo preferito della RANA.",
+            "Quando la RANA è giovane, si chiama girino."
+        ],
+        es: [
+            "La RANA salta en la charca.",
+            "Las moscas son la comida favorita de la RANA.",
+            "Cuando la RANA es joven, se llama renacuajo."
+        ]
+    },
+    HORSE: {
+        en: [
+            "The HORSE is galloping in the field.",
+            "Some humans ride a HORSE.",
+            "Carrots are the HORSE's favorite food."
+        ],
+        fr: [
+            "Le CHEVAL galope dans le champ.",
+            "Certains humains montent à CHEVAL.",
+            "Les carottes sont la nourriture préférée du CHEVAL."
+        ],
+        de: [
+            "Das PFERD galoppiert auf der Weide.",
+            "Einige Menschen reiten auf einem PFERD.",
+            "Karotten sind das Lieblingsessen des PFERDS."
+        ],
+        it: [
+            "Il CAVALLO galoppa nel campo.",
+            "Alcuni umani cavalcano un CAVALLO.",
+            "Le carote sono il cibo preferito del CAVALLO."
+        ],
+        es: [
+            "El CABALLO galopa en el campo.",
+            "Algunos humanos montan a CABALLO.",
+            "Las zanahorias son la comida favorita del CABALLO."
+        ]
+    },
+    SHEEP: {
+        en: [
+            "The SHEEP is being sheared.",
+            "Farmers raise SHEEP for their wool.",
+            "The SHEEP is grazing in the field."
+        ],
+        fr: [
+            "La BREBIS est en train d'être tondue.",
+            "Les agriculteurs élèvent des BREBIS pour leur laine.",
+            "La BREBIS broute dans le champ."
+        ],
+        de: [
+            "Das SCHAF wird geschoren.",
+            "Bauern züchten SCHAFE für ihre Wolle.",
+            "Das SCHAF grast auf der Weide."
+        ],
+        it: [
+            "La PECORA viene tosata.",
+            "Gli agricoltori allevano PECORE per la loro lana.",
+            "La PECORA sta pascolando nel campo."
+        ],
+        es: [
+            "La OVEJA está siendo esquilada.",
+            "Los agricultores crían OVEJAS por su lana.",
+            "La OVEJA pasta en el campo."
+        ]
+    }
+};
