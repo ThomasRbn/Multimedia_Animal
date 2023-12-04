02fa95af173379145fc4fa5bf99951b64d731d37
Azuras03 <nr03russo@gmail.com>
2023-12-04 14:55:53 +0100

    Ajout des traduction pour les animaux

--- src/ConstantsSentenceGame.js
+++ src/ConstantsSentenceGame.js
@@ -1,15 +1,35 @@
 const CAT_SENTENCES = [
     "The CAT is playing with a ball of yarn.",
-    "The female CAT is peacefully sleeping on the couch.",
-    "The CAT scratches the scratching post.",
     "The CAT loves to bask in the sun.",
     "My CAT enjoy hunting mice."
 ]
 
 let CHICKEN_SENTENCES = [
-    "The chicken is clucking in the backyard.",
-    "A rooster crows at dawn.",
+    "The CHICKEN is clucking in the backyard.",
     "Chickens peck at the grains on the ground.",
-    "The chicken lays eggs.",
-    "The habitat of chickens is a chicken coop."
+    "The CHICKEN lays eggs.",
+];
+
+let DINOSAUR_SENTENCES = [
+    "The DINOSAUR is a prehistoric animal.",
+    "The humans and the DINOSAUR did not live at the same time.",
+    "The DINOSAUR has tiny arms.",
+];
+
+let FROG_SENTENCES = [
+    "The FROG is jumping in the pond.",
+    "Flies are the FROG's favorite food.",
+    "When the FROG is young, it is called a tadpole.",
+];
+
+let HORSE_SENTENCES = [
+    "The HORSE is galloping in the field.",
+    "Some humans ride a HORSE.",
+    "Carrots are the HORSE's favorite food.",
+];
+
+let SHEEP_SENTENCES = [
+    "The SHEEP is being sheared.",
+    "Farmers raise SHEEP for their wool.",
+    "The SHEEP is grazing in the field.",
 ];
